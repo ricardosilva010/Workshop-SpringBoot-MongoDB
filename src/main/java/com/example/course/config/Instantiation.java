f857f65686744e34f047ce7d31d66408788e6f82
Ricardo Silva <p.ricardo.silva10@gmail.com>
2024-02-03 13:35:11 +0000

    Projection of author data with DTO

--- src/main/java/com/example/course/config/Instantiation.java
+++ src/main/java/com/example/course/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.example.course.config;
 
 import com.example.course.domain.Post;
 import com.example.course.domain.User;
+import com.example.course.dto.AuthorDTO;
 import com.example.course.repository.PostRepository;
 import com.example.course.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.util.Arrays;
 import java.util.TimeZone;
 
@@ -35,10 +35,11 @@ public class Instantiation implements CommandLineRunner
         User user2 = new User(null, "Alex Green", "alex@gmail.com");
         User user3 = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, simpleDateFormat.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", user1);
-        Post post2 = new Post(null, simpleDateFormat.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", user1);
-
         userRepository.saveAll(Arrays.asList(user1, user2, user3));
+
+        Post post1 = new Post(null, simpleDateFormat.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(user1));
+        Post post2 = new Post(null, simpleDateFormat.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(user1));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ src/main/java/com/example/course/dto/AuthorDTO.java
@@ -0,0 +1,41 @@
+package com.example.course.dto;
+
+import com.example.course.domain.User;
+
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable
+{
+    private String id;
+    private String name;
+
+    public AuthorDTO()
+    {
+    }
+
+    public AuthorDTO(User user)
+    {
+        id = user.getId();
+        name = user.getName();
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
