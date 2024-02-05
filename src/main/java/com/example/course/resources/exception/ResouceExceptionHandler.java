837b318298ef0fafeeeba07560325eee5224f1e6
Ricardo Silva <p.ricardo.silva10@gmail.com>
2024-02-05 15:30:21 +0000

    Simple query with @Query

--- src/main/java/com/example/course/resources/exception/ResouceExceptionHandler.java
+++ src/main/java/com/example/course/resources/exception/ResouceExceptionHandler.java
@@ -7,8 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.Instant;
-
 @ControllerAdvice
 public class ResouceExceptionHandler
 {
