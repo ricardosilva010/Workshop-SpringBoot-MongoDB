899a1822ec18ac3025440346aa687dad23423924
Ricardo Silva <p.ricardo.silva10@gmail.com>
2024-02-05 11:42:08 +0000

    Adding comments to posts

--- src/main/java/com/example/course/config/Instantiation.java
+++ src/main/java/com/example/course/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.example.course.config;
 import com.example.course.domain.Post;
 import com.example.course.domain.User;
 import com.example.course.dto.AuthorDTO;
+import com.example.course.dto.CommentDTO;
 import com.example.course.repository.PostRepository;
 import com.example.course.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner
 
         Post post1 = new Post(null, simpleDateFormat.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(user1));
         Post post2 = new Post(null, simpleDateFormat.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(user1));
+
+        CommentDTO comment1 = new CommentDTO("Boa viagem mano!", simpleDateFormat.parse("21/03/2018"), new AuthorDTO(user2));
+        CommentDTO comment2 = new CommentDTO("Aproveite!", simpleDateFormat.parse("22/03/2018"), new AuthorDTO(user3));
+        CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", simpleDateFormat.parse("23/03/2018"), new AuthorDTO(user2));
+
+        post1.getComments().addAll(Arrays.asList(comment1, comment2));
+        post2.getComments().add(comment3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/example/course/domain/Post.java
+++ src/main/java/com/example/course/domain/Post.java
@@ -1,11 +1,14 @@
 package com.example.course.domain;
 
 import com.example.course.dto.AuthorDTO;
+import com.example.course.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -17,6 +20,8 @@ public class Post implements Serializable
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post()
     {
@@ -79,6 +84,16 @@ public class Post implements Serializable
     public void setAuthor(AuthorDTO author)
     {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments()
+    {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments)
+    {
+        this.comments = comments;
     }
 
     @Override
