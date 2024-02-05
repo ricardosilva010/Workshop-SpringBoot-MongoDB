743edb64ddd423ff5f4abc8f4f6213cb70b32d5c
Ricardo Silva <p.ricardo.silva10@gmail.com>
2024-02-05 12:30:41 +0000

    Simple query with query methods

--- src/main/java/com/example/course/resources/PostResource.java
+++ src/main/java/com/example/course/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.example.course.resources;
 
 import com.example.course.domain.Post;
+import com.example.course.resources.util.URL;
 import com.example.course.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -18,5 +21,13 @@ public class PostResource
     {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text)
+    {
+        text = URL.decodeParameter(text);
+        List<Post> posts = postService.findByTitle(text);
+        return ResponseEntity.ok().body(posts);
     }
 }
--- src/main/java/com/example/course/services/PostService.java
+++ src/main/java/com/example/course/services/PostService.java
@@ -20,4 +20,9 @@ public class PostService
         Optional<Post> post = postRepository.findById(id);
         return post.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
+
+    public List<Post> findByTitle(String text)
+    {
+        return postRepository.findByTitleContainingIgnoreCase(text);
+    }
 }
